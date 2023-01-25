e70735cb2a0548b8197eebe13bfdce5a3ed5173b
JavierReyes <javjreyes@gmail.com>
2023-01-25 14:57:34 -0600

    replace function myLibrary with class myLibrary. Impliment addBookToLibrary to read form data, create new book obj, and add to myLibrary array. Impliment book class with constructor that stores book info in object. Impliment init fuction called by onload even listener that initialized global library variable.

--- myLibrary.js
+++ myLibrary.js
@@ -1,9 +1,29 @@
-let myLibrary=[];
-
-function book(){
-
+class myLibrary{
+    
+    constructor(){
+        this.myLibrary=[];
+    }
+    
+    addBookToLibrary(){
+        let form=document.getElementById('form');
+        let newBookData= new FormData(form);
+        let newBook=new book(newBookData.get("name"),newBookData.get("author"),newBookData.get("format"));
+        this.myLibrary.append(newBook);
+    }
 }
 
-function addBookToLibrary(){
-    
+//book objects contain key information about the book
+class book {
+    constructor(name, author, format) {
+        this.name = name;
+        this.author = author;
+        this.format = format;
+    }
+}
+
+window.addEventListener('load', function() {init();});
+
+var library;
+function init(){
+    library=new myLibrary();
 }
