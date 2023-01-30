c2acd75f0da54e6558fca865cb5d2d919f06496f
JavierReyes <javjreyes@gmail.com>
2023-01-30 12:31:51 -0600

    add mark read button to book cards

--- myLibrary.js
+++ myLibrary.js
@@ -45,10 +45,10 @@ class myLibrary{
         newBook.append(formatP);
 
 
-        /*let editButton=document.createElement("button");
-        editButton.innerText="edit";
-        //editButton.addEventListener('click', function(){});
-        newBook.append(editButton)*/
+        let readButton=document.createElement("button");
+        readButton.innerText="read";
+        readButton.addEventListener('click', function(){library.markRead(this)});
+        newBook.append(readButton)
 
         let delButton=document.createElement("button");
         delButton.innerHTML="<img src='delete.png' alt='delete'>"
